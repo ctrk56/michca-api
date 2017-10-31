a3b1964dbf751be959feb89593560636b7910f47
Cherukuri, Raja K (Contingent Worker) <RKCherukuri@comerica.com>
2017-10-31 16:52:19 -0400

    diamond not supported in 1.7 fix

--- src/main/java/com/ctrk/michca/util/Firebase.java
+++ src/main/java/com/ctrk/michca/util/Firebase.java
@@ -1,5 +1,10 @@
 package com.ctrk.michca.util;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 
 import org.springframework.http.HttpEntity;
@@ -18,21 +23,21 @@ public class Firebase {
 	private RestTemplate restTemplate = null;
 
 	Firebase() {
-//		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-//		Proxy proxy = new Proxy(Type.HTTP, new InetSocketAddress("wwwproxy03.mi.us.comerica.net", 8080));
-//		requestFactory.setProxy(proxy);
-//
-//		restTemplate = new RestTemplate(requestFactory);
-//		
-//		try {
-//			SSLUtil.turnOffSslChecking();
-//		} catch (KeyManagementException e) {
-//			e.printStackTrace();
-//		} catch (NoSuchAlgorithmException e) {
-//			e.printStackTrace();
-//		}
-		restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
-		restTemplate.setInterceptors(Collections.singletonList(new RequestLoggingInterceptor()));
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		Proxy proxy = new Proxy(Type.HTTP, new InetSocketAddress("wwwproxy03.mi.us.comerica.net", 8080));
+		requestFactory.setProxy(proxy);
+
+		restTemplate = new RestTemplate(requestFactory);
+		
+		try {
+			SSLUtil.turnOffSslChecking();
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+//		restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+//		restTemplate.setInterceptors(Collections.singletonList(new RequestLoggingInterceptor()));
 	}
 
 	public User getData(String url) {
@@ -50,7 +55,7 @@ public class Firebase {
 		headers.set("content-type", "application/json");
 		headers.set("x-apikey", "59f88e22741783cb062d807f");
 		headers.set("cache-control", "no-cache");
-		HttpEntity<User> request = new HttpEntity<>(user, headers);
+		HttpEntity<User> request = new HttpEntity<User>(user, headers);
 		restTemplate.postForObject("https://michca-114b.restdb.io/rest/michca-2017", request, User.class);
 	}
 }
